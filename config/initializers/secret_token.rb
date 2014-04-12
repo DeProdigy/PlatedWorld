30c756581f10b8f72d07d832212add7be4fba650
DeProdigy <alex.hint.ah@gmail.com>
2014-04-12 18:33:14 -0400

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlatedWorld::Application.config.secret_token = '17fc4e13e3a31b439a5a69844393aa2bff32f103e45981d6f0411a388e8562faa8c2c9ab19a9ef2f41905d9a2bc41342a49bb19057807bc12b5696856f30b0cc'
