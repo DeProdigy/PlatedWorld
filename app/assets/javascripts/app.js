33fdb1ad280cf132e7420339fd6846ee764b9d61
DeProdigy <alex.hint.ah@gmail.com>
2014-04-13 17:45:33 -0400

    set up a new structure for the app and make it all object oriented

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,23 @@
+var app = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getDishes() {
+//   $.getJSON('/dishes', function(result) {
+//     drawMarkers(result);
+//   });
+// }
+
+// function drawMarkers(result) {
+//   console.log(result);
+// }
---
+++ app/assets/javascripts/controller.js
@@ -0,0 +1,6 @@
+$(document).ready(function($) {
+  //create a map when dom loads
+  google.maps.event.addDomListener(window, 'load', app.map.initialize);
+
+
+});
