3bb2a5282153e2eedea109cd1a53d59f0d832098
DeProdigy <alex.hint.ah@gmail.com>
2014-04-12 21:13:11 -0400

    add config.assets.initialize_on_precompile = false to application.rb

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,6 +14,10 @@ $(document).ready(function($) {
 
 function getDishes() {
   $.getJSON('/dishes', function(result) {
-    console.log(result);
+    drawMarkers(result);
   });
+}
+
+function drawMarkers(result) {
+  console.log(result);
 }
