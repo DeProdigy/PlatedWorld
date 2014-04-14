3bd1a032ff5685f62cb0746ace8b8afbeada94a6
DeProdigy <alex.hint.ah@gmail.com>
2014-04-14 01:44:48 -0400

    customize map colors to be like plated

--- app/assets/javascripts/visual.js
+++ app/assets/javascripts/visual.js
@@ -1,7 +1,7 @@
 app.visual = {
 
   initialize: function() {
-    var styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#333739"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#CE0000"}]},{"featureType":"poi","stylers":[{"color":"#CE0000"},{"lightness":-7}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-28}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#CE0000"},{"visibility":"on"},{"lightness":-15}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-18}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-34}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#333739"},{"weight":0.8}]},{"featureType":"poi.park","stylers":[{"color":"#CE0000"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#333739"},{"weight":0.3},{"lightness":10}]}];
+    var styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#FFFFFF"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#CE0000"}]},{"featureType":"poi","stylers":[{"color":"#CE0000"},{"lightness":-7}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-28}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#CE0000"},{"visibility":"on"},{"lightness":-15}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-18}]},{"elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-34}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#FFFFFF"},{"weight":0.8}]},{"featureType":"poi.park","stylers":[{"color":"#CE0000"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#FFFFFF"},{"weight":0.3},{"lightness":10}]}];
 
     //simple red
     // var styles = [
@@ -14,7 +14,7 @@ app.visual = {
     // ];
 
     var mapOptions = {
-      center: new google.maps.LatLng(20, -20),
+      center: new google.maps.LatLng(30, -20),
       zoom: 3,
       styles: styles
     };
