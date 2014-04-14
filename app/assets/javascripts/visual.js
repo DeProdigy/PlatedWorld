7fa56666e7501b3e48b0f27f597bed1d6da4e47f
DeProdigy <alex.hint.ah@gmail.com>
2014-04-14 01:31:09 -0400

    change map

--- app/assets/javascripts/visual.js
+++ app/assets/javascripts/visual.js
@@ -1,28 +1,17 @@
 app.visual = {
 
   initialize: function() {
-    var styles = [
-      {
-        stylers: [
-          { hue: "#CE0000" },
-          { saturation: -40 }
-        ]
-      }
-      // ,{
-      //   featureType: "road",
-      //   elementType: "geometry",
-      //   stylers: [
-      //     { lightness: 50 },
-      //     { visibility: "simplified" }
-      //   ]
-      // },{
-      //   featureType: "road",
-      //   elementType: "labels",
-      //   stylers: [
-      //     { visibility: "off" }
-      //   ]
-      // }
-    ];
+    var styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#333739"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#CE0000"}]},{"featureType":"poi","stylers":[{"color":"#CE0000"},{"lightness":-7}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-28}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#CE0000"},{"visibility":"on"},{"lightness":-15}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-18}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#CE0000"},{"lightness":-34}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#333739"},{"weight":0.8}]},{"featureType":"poi.park","stylers":[{"color":"#CE0000"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#333739"},{"weight":0.3},{"lightness":10}]}];
+
+    //simple red
+    // var styles = [
+    //   {
+    //     stylers: [
+    //       { hue: "#CE0000" },
+    //       { saturation: -40 }
+    //     ]
+    //   }
+    // ];
 
     var mapOptions = {
       center: new google.maps.LatLng(20, -20),
