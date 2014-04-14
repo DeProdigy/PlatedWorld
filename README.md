5f25a3da8be9c4f18b2417670cfe66d763f89ad6
DeProdigy <alex.hint.ah@gmail.com>
2014-04-14 04:35:01 -0400

    move the map off the screen to hide the google tools

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ add 5 more dishes to DB
 
 take away the standard google tools
 
-skin the map
-
 make the image background of the card
 
 style cards
 
-map an intro card
+make an intro card
 
 use the logo
 
--- app/assets/javascripts/visual.js
+++ app/assets/javascripts/visual.js
@@ -1,24 +1,15 @@
 app.visual = {
 
   initialize: function() {
+    // create cutom map colors
     var styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#BCD4DF"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#E94B35"}]},{"featureType":"poi","stylers":[{"color":"#E94B35"},{"lightness":-7}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#E94B35"},{"lightness":-28}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#E94B35"},{"visibility":"on"},{"lightness":-15}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#E94B35"},{"lightness":-18}]},{"elementType":"labels.text.fill","stylers":[{"color":"#2A3A42"}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#E94B35"},{"lightness":-34}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#BCD4DF"},{"weight":0.8}]},{"featureType":"poi.park","stylers":[{"color":"#E94B35"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#BCD4DF"},{"weight":0.3},{"lightness":10}]}];
 
-    //simple red
-    // var styles = [
-    //   {
-    //     stylers: [
-    //       { hue: "#CE0000" },
-    //       { saturation: -40 }
-    //     ]
-    //   }
-    // ];
-
     var mapOptions = {
-      center: new google.maps.LatLng(30, -20),
+      center: new google.maps.LatLng(25, -20),
       zoom: 3,
       styles: styles
     };
-    // debugger;
+
     app.visual.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   },
 
@@ -29,8 +20,7 @@ app.visual = {
       geocoder.geocode( { 'address': country }, function(results, status) {
 
         if (status == google.maps.GeocoderStatus.OK) {
-          //center the map over the last marker
-          // app.visual.map.setCenter(results[0].geometry.location);
+
           var marker = new google.maps.Marker({
             map: app.visual.map,
             position: results[0].geometry.location,
